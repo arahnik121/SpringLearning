cbc88e9e92d5e9c53099f157213d200f8e9cdb6a
arahnik121 <arahnik121@yandex.ru>
2020-07-16 23:52:12 +0300

    IoC realization example in MusicPlayer.java

---
+++ src/main/java/ru/spring/test/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package ru.spring.test;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/ru/spring/test/Music.java
@@ -0,0 +1,5 @@
+package ru.spring.test;
+
+public interface Music {
+    String getSong();
+}
