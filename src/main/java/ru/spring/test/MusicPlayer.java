8ce37360d74567c691d8e30df01945725144e233
arahnik121 <arahnik121@yandex.ru>
2020-07-30 12:42:58 +0300

    Using @Component annotation

--- src/main/java/ru/spring/test/MusicPlayer.java
+++ src/main/java/ru/spring/test/MusicPlayer.java
@@ -1,57 +1,14 @@
 package ru.spring.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MusicPlayer {
-
-    public MusicPlayer(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
-    public List<Music> getMusicList() {
-        return musicList;
-    }
-
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
-
-    private List<Music> musicList = new ArrayList<>();
-
-    private String name;
-    private int volume;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
+    private Music music;
 
     //IoC
-//    public MusicPlayer(Music music) {
-//        this.music = music;
-//    }
-
-    public MusicPlayer() {}
-
-//    public void setMusic(Music music) {
-//        this.music = music;
-//    }
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
 
     public void playMusic() {
-        for (Music music : musicList) {
-            System.out.println("Playing " + music.getSong());
-        }
+        System.out.println("Playing " + music.getSong());
     }
 }
