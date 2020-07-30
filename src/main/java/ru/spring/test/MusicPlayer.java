1d3d20e03affa20c826c5df2b3de067bd6da3d4b
arahnik121 <arahnik121@yandex.ru>
2020-07-30 13:15:02 +0300

    Using @Autowired annotation for multiple DI

--- src/main/java/ru/spring/test/MusicPlayer.java
+++ src/main/java/ru/spring/test/MusicPlayer.java
@@ -5,19 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
     @Autowired
-    public void setMusic(Music music) {
-        this.music = music;
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
     }
 }
--- src/main/java/ru/spring/test/TestSpring.java
+++ src/main/java/ru/spring/test/TestSpring.java
@@ -16,8 +16,11 @@ public class TestSpring {
 //        musicPlayer.playMusic();
 //        classicalMusicPlayer.playMusic();
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
